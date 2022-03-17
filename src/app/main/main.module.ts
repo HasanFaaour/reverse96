694584a3122c7b2c3a367d4e295b9148479b4b9b
HasanFaaour <1abo1995ali@gmail.com>
2022-03-17 14:10:55 +0330

    create side-bar

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,6 +5,10 @@ import { MainComponent } from './container/main/main.component';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 const routes: Routes = [
   {
@@ -17,10 +21,12 @@ const routes: Routes = [
   declarations: [
     MainComponent,
     TopNavComponent,
-    FooterComponent
+    FooterComponent,
+    SideBarComponent,
   ],
   imports: [
     CommonModule,
+    MatIconModule,
     RouterModule,
     RouterModule.forChild(routes),
   ]
