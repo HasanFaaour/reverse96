aaf98dab252f2bd3768f7685011b1d54f6caa29a
ArashDz <adashti7799@gmail.com>
2022-03-18 01:04:42 +0330

    Implement Login and Signup

--- src/app/main/components/login/login.component.ts
+++ src/app/main/components/login/login.component.ts
@@ -1,4 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpRequestService } from '../../../http-service.service';
+import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
+import { Validators, ValidatorFn, ValidationErrors } from '@angular/forms';
+
+
+//Creating a Custom Validator For Username or Email Field
+function validateUserMail(): ValidatorFn{
+  return (control : AbstractControl) : ValidationErrors | null => {
+    if (control.value.includes("@")){
+      return Validators.email(control);
+    }else{
+      if (control.value.length < 3)
+        return {minlength : control.value};
+      else
+        return control.value.length > 15?{maxlength : control.value}:null;
+    }
+  }
+}
 
 @Component({
   selector: 'app-login',
@@ -7,9 +25,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private Request : HttpRequestService) { }
 
   ngOnInit(): void {
   }
+
+  //Defining ... Flags
+  userEnteredEmail = false;
+  loggedInUsername = "@N/A";
+  wrongPassword = false;
+  token = "N/A";
+
+
+  //Defining Reactive Forms
+  loginCredentials = new FormGroup({
+    usermail : new FormControl('',[Validators.required, validateUserMail()]),
+    password : new FormControl('',[Validators.required, Validators.minLength(6), Validators.maxLength(24)])
+  });
+
+  //Lookin for the '@' character in the "username or e-mail" input to determine if the user is trying to enter a username or a password 
+  checkUserEntry():void{
+    if (this.usermail?.value.includes("@")){
+      this.userEnteredEmail = true;
+    }else{
+      this.userEnteredEmail = false;
+    }
+    this.wrongPassword = false;
+    return;
+  }
+
+  //Defining the submit method to handle the request
+  submit ():void{
+    let response = this.Request.login(this.loginCredentials.value); 
+    if ("access" in response){
+      console.log("sucess");
+      this.loggedInUsername = this.loginCredentials.value['usermail'];
+      console.log(response);
+      return;
+    }else{
+      console.log("wrong!");
+      this.wrongPassword = true;
+      return;
+    }
+  }
+
+  //Defining getter methods for easier acess to the reactive form inputs
+  get usermail() {
+    return this.loginCredentials.get("usermail");
+  }
+
+  get password() {
+    return this.loginCredentials.get("password");
+  }
+
+
+
+
+
 
 }
--- src/app/main/components/signup/signup.component.ts
+++ src/app/main/components/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { HttpRequestService } from '../../../http-service.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private Request : HttpRequestService) { }
 
   ngOnInit(): void {
+  }
+
+  //Defining reactive forms
+  signupCredentials = new FormGroup({
+    name : new FormControl('',[Validators.minLength(3), Validators.maxLength(15)]),
+    address : new FormControl('',[Validators.minLength(10), Validators.maxLength(115)]),
+    phonenumber : new FormControl('',[Validators.minLength(11), Validators.maxLength(11), Validators.pattern("09[0-9]*")]),
+    email : new FormControl('',[Validators.required, Validators.email]),
+    username : new FormControl('',[Validators.required, Validators.minLength(3), Validators.maxLength(15)]),
+    password : new FormControl('',[Validators.required, Validators.minLength(6), Validators.maxLength(24)])
+  });
+
+  //Defining the submit method to handle the request
+  submit() :void {
+    this.Request.signup(this.signupCredentials.value);
+    return;
+  }
+
+  //Defining getter methods for easier access to reactive form inputs
+  get name(){
+    return this.signupCredentials.get("name")
+  }
+
+  get address(){
+    return this.signupCredentials.get("address")
+  }
+
+  get phone(){
+    return this.signupCredentials.get("phonenumber")
+  }
+
+  get email(){
+    return this.signupCredentials.get("email")
+  }
+
+  get username(){
+    return this.signupCredentials.get("username")
+  }
+
+  get password(){
+    return this.signupCredentials.get("password")
   }
 
 }
