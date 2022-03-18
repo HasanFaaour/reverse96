93bb780d35c0bd05c4b77a9f170e66c2920c927c
HasanFaaour <1abo1995ali@gmail.com>
2022-03-18 14:48:39 +0330

    create home component

--- src/app/main/container/main/main.component.ts
+++ src/app/main/container/main/main.component.ts
@@ -9,21 +9,6 @@ export class MainComponent implements OnInit {
 
   constructor() { }
   title = 'new-app';
-  isClicked: boolean = false;
-  enaColor: boolean = false;
-  likeNum: number = 10;
-
-  onClick() {
-    this.isClicked = !this.isClicked;
-    this.enaColor = !this.enaColor;
-    if(this.enaColor == false)
-    this.likeNum = this.likeNum - 1;
-    else
-    this.likeNum = this.likeNum + 1;
-  }
-  decLike(){
-    this.likeNum = - this.likeNum;
-  }
   ngOnInit(): void {
   }
 
